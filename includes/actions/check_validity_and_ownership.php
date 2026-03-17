ff83ebef76afdb0c9dd13a3ad29ea6fc44e91920
Bernhard Kau <github@kau-boys.de>
2026-03-17 18:55:23 +0100

    Fix CVE-2025-50039

--- includes/actions/check_validity_and_ownership.php
+++ includes/actions/check_validity_and_ownership.php
@@ -2,6 +2,10 @@
 use WP_VGWORT\Metabox;
 
 function metabox_check_validity_and_ownership() {
+    if ( ! current_user_can( 'edit_posts' ) ) {
+        wp_send_json_error( [ 'message' => esc_html__( 'Permission denied', 'vgw-metis' ) ], 400 );
+    }
+
     $plugin = vgw_metis_get_instance();
     $metaBox = new Metabox( $plugin );
     $metaBox->is_valid_and_ownership_check();
--- includes/actions/get_posts_count.php
+++ includes/actions/get_posts_count.php
@@ -1,6 +1,9 @@
 <?php
 use WP_VGWORT\Db_Pixels;
 function get_posts_count() {
+    if ( ! current_user_can( 'edit_posts' ) ) {
+        wp_send_json_error( [ 'message' => esc_html__( 'Permission denied', 'vgw-metis' ) ], 403 );
+    }
 
     // Check for nonce security
     if (!check_ajax_referer('wp_metis_metabox_nonce', 'security', false)) {
