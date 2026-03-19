6c310c23602e9acdbf2e9210a4ccbce8158fa615
Bernhard Kau <github@kau-boys.de>
2026-03-19 18:00:53 +0100

    Load admin CSS only in the backend

--- includes/actions/include_assets.php
+++ includes/actions/include_assets.php
@@ -1,10 +1,12 @@
 <?php
 function vgw_metis_sidebar_enqueue_assets() {
-    wp_enqueue_style(
-        'vgw-metis-sidebar-style',
-        plugin_dir_url(__FILE__) . '../../admin/css/vgw-metis-sidebar.css', // Adjust the path to your CSS file
-        array(),
-        '1.0'
-    );
+    if ( is_admin() ) {
+        wp_enqueue_style(
+            'vgw-metis-sidebar-style',
+            plugin_dir_url( __FILE__ ) . '../../admin/css/vgw-metis-sidebar.css', // Adjust the path to your CSS file
+            array(),
+            '1.0'
+        );
+    }
 }
 add_action('enqueue_block_assets', 'vgw_metis_sidebar_enqueue_assets');
